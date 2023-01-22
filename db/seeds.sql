5b5550557a82309385619695ffd0566ec4de7df4
Laura Trapaga <trapaga9@gmail.com>
2023-01-21 20:43:09 -0500

    Add seeds.sql code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,26 @@
+INSERT INTO departments (department_name)
+VALUES ("Engineering"),
+       ("Legal"),
+       ("Finance")
+       ("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 150000, 1 ),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 3 ),
+       ("Accountant", 125000, 3)
+       ("Legal Team Lead", 250000, 2);
+       ("Lawyer", 190000, 2);
 
 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, null),
+       ('Mike', 'Chan', 2, 1),
+       ('Ashley', 'Rodriguez', 3, null),
+       ('Laura', 'Trapaga', 4, 3),
+       ('Kunal', 'Singh', 5, null),
+       ('Malia', 'Brown', 6, 5),
+       ('Sarah', 'Lourd', 7, null),
+       ('Calvin', 'Chern', 8, 7);
